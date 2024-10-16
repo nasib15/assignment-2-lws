b80a0f2479c43cf01af788caf0a5ff42e0f2414c
nasib15 <nasibhossain57@gmail.com>
2024-10-16 20:54:24 +0600

    Add: Balance sheet section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Form from "./components/Navbar/Form";
-import Navbar from "./components/Navbar/Navbar";
+import Form from "./components/Form";
+import Navbar from "./components/Navbar";
+import RightSideColumn from "./components/RightSideColumn";
 
 function App() {
   return (
@@ -7,7 +8,10 @@ function App() {
       <Navbar />
       <main className="relative mx-auto mt-10 w-full max-w-7xls">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"></section>
+        {/* Left column */}
         <Form />
+        {/* Right column */}
+        <RightSideColumn />
       </main>
     </>
   );
---
+++ src/components/RightSideColumn.jsx
@@ -0,0 +1,10 @@
+import BalanceSheet from "./BalanceSheet";
+
+const RightSideColumn = () => {
+  return (
+    <div className="lg:col-span-2">
+      <BalanceSheet />
+    </div>
+  );
+};
+export default RightSideColumn;
